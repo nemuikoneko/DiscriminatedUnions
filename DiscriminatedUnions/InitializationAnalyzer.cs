d67052204f1855dc4b574209708cbdbbc7a29ae4
Nemui <80854482+nemuikoneko@users.noreply.github.com>
2022-07-23 14:08:07 +0200

    Added barebones analyzer

---
+++ DiscriminatedUnions/InitializationAnalyzer.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+using System.Collections.Immutable;
+
+namespace DiscriminatedUnions;
+
+[DiagnosticAnalyzer(LanguageNames.CSharp)]
+public sealed class InitializationAnalyzer : DiagnosticAnalyzer
+{
+    public override ImmutableArray<DiagnosticDescriptor> SupportedDiagnostics
+        => throw new System.NotImplementedException();
+
+    public override void Initialize(AnalysisContext context)
+    {
+        context.EnableConcurrentExecution();
+        context.ConfigureGeneratedCodeAnalysis(GeneratedCodeAnalysisFlags.Analyze | GeneratedCodeAnalysisFlags.ReportDiagnostics);
+    }
+}
