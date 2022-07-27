603d58f92cb78dbb69c35cb1faa8ddf2edf12117
Nemui <80854482+nemuikoneko@users.noreply.github.com>
2022-07-27 16:32:01 +0200

    Added basic code fix template

---
+++ DiscriminatedUnions/DiscriminatedUnionCodeFixProvider.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.CodeAnalysis.CodeFixes;
+using System.Collections.Immutable;
+using System.Threading.Tasks;
+
+namespace nemuikoneko.DiscriminatedUnions;
+
+internal class DiscriminatedUnionCodeFixProvider : CodeFixProvider
+{
+    public override ImmutableArray<string> FixableDiagnosticIds => throw new System.NotImplementedException();
+
+    public override Task RegisterCodeFixesAsync(CodeFixContext context)
+    {
+        throw new System.NotImplementedException();
+    }
+}
