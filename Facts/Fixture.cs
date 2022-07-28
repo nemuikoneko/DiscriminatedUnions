99acdab870672909fe4625aa8be873e66bb19cad
Nemui <80854482+nemuikoneko@users.noreply.github.com>
2022-07-28 10:17:45 +0200

    Moved test setup code to a shared place

--- Facts/Fixture.cs
+++ Facts/Fixture.cs
@@ -1,10 +1,16 @@
 ﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
 using nemuikoneko.DiscriminatedUnions;
 using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Linq;
 using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Facts;
 
@@ -22,5 +28,69 @@ internal sealed class Fixture
         var generatorDriver = CSharpGeneratorDriver.Create(new SourceGenerator());
         generatorDriver.RunGeneratorsAndUpdateCompilation(inputCompilation, out var outputCompilation, out var diagnostics);
         return (outputCompilation, diagnostics.ToList());
+    }
+
+    internal static async Task SetUpEnvironmentAsync(SyntaxTree syntaxTree)
+    {
+        var project = await CreateProjectWithSingleDocumentAsync(syntaxTree);
+
+        var orgCompilation = await GetCompilationAsync(project);
+        var compilationAfterRunningGenerator = RunGenerator(orgCompilation.compilation);
+        var compilationAfterRunningAnalyzer = await RunAnalyzer(compilationAfterRunningGenerator.compilation);
+
+        foreach (var diagnostic in compilationAfterRunningAnalyzer.diagnostics)
+        {
+            var changedDocuments = await RunCodeFixProvider(project.Documents.Single(), diagnostic);
+        }
+    }
+
+    private static async Task<(Compilation compilation, List<Diagnostic> diagnostics)> GetCompilationAsync(Project project)
+    {
+        var compilation = await project.GetCompilationAsync();
+        if (compilation == null)
+            throw new Exception("Failed to retrieve compilation");
+
+        var diagnostics = compilation.GetDiagnostics().ToList();
+
+        return (compilation, diagnostics);
+    }
+
+    private static async Task<(CompilationWithAnalyzers compilation, List<Diagnostic> diagnostics)> RunAnalyzer(Compilation compilation)
+    {
+        var compilationWithAnalyzers = compilation.WithAnalyzers(ImmutableArray.Create<DiagnosticAnalyzer>(new Analyzer()));
+        var diagnostics = (await compilationWithAnalyzers.GetAllDiagnosticsAsync()).ToList();
+        return (compilationWithAnalyzers, diagnostics);
+    }
+
+    private static async Task<Project> CreateProjectWithSingleDocumentAsync(SyntaxTree syntaxTree)
+        => new AdhocWorkspace()
+            .CurrentSolution
+            .AddProject(ProjectId.CreateNewId(), "ProjectName", "AssemblyName", LanguageNames.CSharp)
+            .Projects
+            .Single()
+            .AddDocument("Document.cs", await syntaxTree.GetRootAsync())
+            .Project
+            .AddMetadataReferences(
+                Basic.Reference.Assemblies.Net60.All
+                    .Append(MetadataReference.CreateFromFile(typeof(SourceGenerator).Assembly.Location)));
+
+    private static async Task<List<Document>> RunCodeFixProvider(Document document, Diagnostic diagnostic)
+    {
+        var codeActions = new List<CodeAction>();
+        var codeFixContext = new CodeFixContext(document, diagnostic, (a, d) => codeActions.Add(a), CancellationToken.None);
+        await new DiscriminatedUnionCodeFixProvider().RegisterCodeFixesAsync(codeFixContext);
+
+        var changedDocumentTasks = codeActions
+            .Select(async codeAction => await codeAction.GetOperationsAsync(CancellationToken.None))
+            .Select(async operations => (await operations).OfType<ApplyChangesOperation>().Single().ChangedSolution)
+            .Select(async changedSolution => (await changedSolution).Projects.Single().Documents.Single(d => d.Id == document.Id))
+            .ToList();
+
+        var changedDocuments = new List<Document>();
+        foreach (var changedDocumentTask in changedDocumentTasks)
+        {
+            changedDocuments.Add(await changedDocumentTask);
+        }
+        return changedDocuments;
     }
 }
